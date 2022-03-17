4074fb984860cc7bc216edffac2c040011574078
Lucas <lucasazzollinivieira@gmail.com>
2022-03-17 19:56:34 -0300

    feat: delete user short url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -31,3 +31,52 @@ export async function createUrl(req, res) {
         return res.sendStatus(500);
     }
 }
+
+export async function getUrl(req, res) {
+    const { shortUrl } = req.params;
+
+    try {
+        const result = await db.query(`
+            SELECT "shortUrls".id, "shortUrls"."shortUrl", "shortUrls".url FROM "shortUrls" WHERE "shortUrls"=$1
+        `, [shortUrl]);
+
+        if (result.rowCount === 0) {
+            return res.sendStatus(404);
+        }
+
+        res.status(200).send(result.rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function deleteUrl(req, res) {
+    const { id } = req.params;
+
+    const authorization = req.headers.authorization;
+    const token = authorization?.replace("Bearer", "");
+
+    try {
+
+        const users = await db.query(`
+            SELECT "userId" FROM sessions WHERE token=$1
+        `, [token]);
+
+        const { userId } = users.rows[0];
+
+        const url = await db.query(`
+            SELECT id FROM "shortUrls" WHERE id=$1 AND "userId"=$2
+        `, [id, userId]);
+
+        if (url.rowCount === 0) {
+            return res.sendStatus(401);
+        }
+
+        await db.query(`DELETE FROM "shortUrls" WHERE id=$1`, [id]);
+        res.sendStatus(204);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUrl } from "../controllers/urlController.js";
+import { createUrl, deleteUrl, getUrl } from "../controllers/urlController.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 import urlSchema from "../schemas/urlSchema.js";
 
 const urlRouter = Router();
-urlRouter.post('/urls/shorten', validateSchemaMiddleware(urlSchema), createUrl);
+urlRouter.post('/urls/shorten', validateSchemaMiddleware(urlSchema), validateTokenMiddleware, createUrl);
+urlRouter.get('/urls/:shortUrl', getUrl);
+urlRouter.delete('/urls:/id', validateTokenMiddleware, deleteUrl);
 
 export default urlRouter;
